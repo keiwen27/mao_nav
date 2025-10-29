a607897add1f7a9fbb7584fc29f929f608fe9ba3
keiwen27 <32326381+keiwen27@users.noreply.github.com>
2025-10-29 19:24:44 +0800

    chore: 更新导航数据 - 2025/10/29 19:24:41

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -446,14 +446,14 @@ export const mockData = {
         },
         {
           "id": "site-1761730157433",
-          "name": "低效考核",
+          "name": "低效店考核",
           "url": "https://bi.erke.com:8508/webroot/decision/link/63CM",
           "description": "低效考核",
           "icon": ""
         },
         {
           "id": "site-1761730196251",
-          "name": "费用检查",
+          "name": "费用异常检查",
           "url": "https://bi.erke.com:8508/webroot/decision/link/BNgp",
           "description": "费用检查",
           "icon": ""
