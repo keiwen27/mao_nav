a9a126fcd905089b2c1543d20658b07b1602077b
keiwen27 <32326381+keiwen27@users.noreply.github.com>
2025-10-29 17:29:23 +0800

    chore: 更新导航数据 - 2025/10/29 17:29:19

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -429,7 +429,29 @@ export const mockData = {
       "icon": "👥",
       "name": "帆软BI",
       "order": 0,
-      "sites": []
+      "sites": [
+        {
+          "id": "site-1761730050442",
+          "name": "应收坏账计提",
+          "url": "https://bi.erke.com:8508/webroot/decision/link/lcwt",
+          "description": "每月坏账计提",
+          "icon": ""
+        },
+        {
+          "id": "site-1761730098673",
+          "name": "加盟授信",
+          "url": "https://bi.erke.com:8508/webroot/decision/link/HGlT",
+          "description": "加盟客户授信",
+          "icon": ""
+        },
+        {
+          "id": "site-1761730157433",
+          "name": "低效考核",
+          "url": "https://bi.erke.com:8508/webroot/decision/link/63CM",
+          "description": "低效考核",
+          "icon": ""
+        }
+      ]
     }
   ],
   "title": "猪猪导航"
