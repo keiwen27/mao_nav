687f3f29e0d96e6c9af9fbf0fd5dc8392dbf30b0
keiwen27 <32326381+keiwen27@users.noreply.github.com>
2025-10-30 14:44:59 +0800

    chore: 更新导航数据 - 2025/10/30 14:44:58

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -8,28 +8,28 @@ export const mockData = {
       "sites": [
         {
           "id": "site-1761730050442",
-          "name": "应收坏账计提",
+          "name": "坏账",
           "url": "https://bi.erke.com:8508/webroot/decision/link/lcwt",
           "description": "每月坏账计提",
           "icon": ""
         },
         {
           "id": "site-1761730098673",
-          "name": "加盟授信",
+          "name": "授信",
           "url": "https://bi.erke.com:8508/webroot/decision/link/HGlT",
           "description": "加盟客户授信",
           "icon": ""
         },
         {
           "id": "site-1761730157433",
-          "name": "低效店考核",
+          "name": "低效",
           "url": "https://bi.erke.com:8508/webroot/decision/link/63CM",
           "description": "低效考核",
           "icon": ""
         },
         {
           "id": "site-1761730196251",
-          "name": "费用异常检查",
+          "name": "费用",
           "url": "https://bi.erke.com:8508/webroot/decision/link/BNgp",
           "description": "费用检查",
           "icon": ""
