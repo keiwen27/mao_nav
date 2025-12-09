3ffd8d15db4fe1ab1d69387706757dfbde1a272f
keiwen27 <32326381+keiwen27@users.noreply.github.com>
2025-12-09 10:05:20 +0800

    chore: 更新导航数据 - 2025/12/9 10:05:20

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -9,28 +9,28 @@ export const mockData = {
         {
           "id": "site-1761730050442",
           "name": "坏账",
-          "url": "https://bi.erke.com:8508/webroot/decision/link/lcwt",
+          "url": "https://bi.erke.com:8508/webroot2/decision/link/lcwt",
           "description": "每月坏账计提",
           "icon": "https://favicon.im/finebi.com?t=1762506300240"
         },
         {
           "id": "site-1761730098673",
           "name": "授信",
-          "url": "https://bi.erke.com:8508/webroot/decision/link/HGlT",
+          "url": "https://bi.erke.com:8508/webroot2/decision/link/HGlT",
           "description": "加盟客户授信",
           "icon": "https://favicon.im/finebi.com?t=1762506300240"
         },
         {
           "id": "site-1761730157433",
           "name": "低效",
-          "url": "https://bi.erke.com:8508/webroot/decision/link/63CM",
+          "url": "https://bi.erke.com:8508/webroot2/decision/link/63CM",
           "description": "低效考核",
           "icon": "https://favicon.im/finebi.com?t=1762506300240"
         },
         {
           "id": "site-1761730196251",
           "name": "费用",
-          "url": "https://bi.erke.com:8508/webroot/decision/link/BNgp",
+          "url": "https://bi.erke.com:8508/webroot2/decision/link/BNgp",
           "description": "费用检查",
           "icon": "https://favicon.im/finebi.com?t=1762506300240"
         }
