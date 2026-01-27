5026bd38a173f5ff6c68070160731e2e0a5ef276
keiwen27 <32326381+keiwen27@users.noreply.github.com>
2026-01-27 17:22:22 +0800

    chore: 更新导航数据 - 2026/1/27 17:22:22

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -235,42 +235,6 @@ export const mockData = {
       ]
     },
     {
-      "id": "category-1761729951890",
-      "icon": "📊",
-      "name": "帆软看板",
-      "order": 3,
-      "sites": [
-        {
-          "id": "site-1761730050442",
-          "name": "坏账",
-          "url": "https://bi.erke.com:8508/webroot2/decision/link/lcwt",
-          "description": "每月坏账计提",
-          "icon": "https://www.faviconextractor.com/favicon/bi.erke.com:8508"
-        },
-        {
-          "id": "site-1761730098673",
-          "name": "授信",
-          "url": "https://bi.erke.com:8508/webroot2/decision/link/HGlT",
-          "description": "加盟客户授信",
-          "icon": "https://www.faviconextractor.com/favicon/bi.erke.com:8508"
-        },
-        {
-          "id": "site-1761730157433",
-          "name": "低效",
-          "url": "https://bi.erke.com:8508/webroot2/decision/link/63CM",
-          "description": "低效考核",
-          "icon": "https://www.faviconextractor.com/favicon/bi.erke.com:8508"
-        },
-        {
-          "id": "site-1761730196251",
-          "name": "费用",
-          "url": "https://bi.erke.com:8508/webroot2/decision/link/BNgp",
-          "description": "费用检查",
-          "icon": "https://www.faviconextractor.com/favicon/bi.erke.com:8508"
-        }
-      ]
-    },
-    {
       "id": "category-1769072714248",
       "icon": "⚙️",
       "name": "在线工具",
